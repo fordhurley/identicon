bf84e34f9fed431feb0fbea20d2e3aa76dc5ba43
Ford Hurley <ford.hurley@gmail.com>
2019-03-13 17:35:14 -0400

    Play with more color palettes

--- cmd/identicon/main.go
+++ cmd/identicon/main.go
@@ -12,11 +12,7 @@ import (
 func main() {
 	gridSize := 5
 	scale := 32
-	fgs := []color.Color{
-		color.RGBA{50, 178, 255, 255},
-		color.RGBA{163, 172, 177, 255},
-		color.RGBA{0, 0, 0, 255},
-	}
+	fgs := rainbow
 	bg := color.RGBA{255, 255, 255, 255}
 
 	img := identicon.New(os.Stdin, gridSize, scale, fgs, bg)
@@ -24,4 +20,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+var cool = []color.Color{
+	color.RGBA{50, 178, 255, 255},
+	color.RGBA{163, 172, 177, 255},
+	color.RGBA{0, 0, 0, 255},
+}
+
+var rainbow = []color.Color{
+	color.RGBA{170, 255, 0, 255},
+	color.RGBA{255, 170, 0, 255},
+	color.RGBA{255, 0, 170, 255},
+	color.RGBA{170, 0, 255, 255},
+	color.RGBA{0, 170, 255, 255},
 }
