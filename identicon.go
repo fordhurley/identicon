26a975065ed526b37fd2aaa143080c29966c26e8
Ford Hurley <ford.hurley@gmail.com>
2019-03-13 11:01:22 -0400

    Just use the entire palette

--- identicon.go
+++ identicon.go
@@ -1,7 +1,6 @@
 package identicon
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
@@ -10,17 +9,10 @@ import (
 // New generates a new identicon image. numPixels is the side length of the
 // square image in pixels.
 func New(hash []byte, numPixels int, palette color.Palette) (image.Image, error) {
-	const minBytes = 7 // 3 for fg, 3 for bg, and at least 1 for pixel data
-	if len(hash) < minBytes {
-		return nil, fmt.Errorf("identicon: need at least %d bytes of hash", minBytes)
-	}
-
-	fg := palette.Convert(byteColor(hash[0:3]))
-	bg := palette.Convert(byteColor(hash[3:6]))
-	pixelBytes := byteRing{bytes: hash[6:len(hash)]}
+	pixelBytes := byteRing{bytes: hash}
 
 	rect := image.Rect(0, 0, numPixels, numPixels)
-	img := image.NewPaletted(rect, color.Palette{fg, bg})
+	img := image.NewPaletted(rect, palette)
 
 	for _, cell := range grid(rect, pixelBytes.len()) {
 		src := image.NewUniform(byteColor(pixelBytes.nextN(3)))
