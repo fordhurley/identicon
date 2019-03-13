8256e6ff8bc30d7fdc10a5f16573d4dc9713162a
Ford Hurley <ford.hurley@gmail.com>
2019-03-13 15:11:27 -0400

    Simplify

--- identicon.go
+++ identicon.go
@@ -7,59 +7,36 @@ import (
 )
 
 // New generates a new identicon image. gridSize is the number of grid cells
-// horizontally and vertically. scale multiplies the gridSize to size of the
-// image in pixels.
+// horizontally and vertically. scale multiplies the gridSize to size the image
+// in pixels. The final image will be [gridSize*scale x gridSize*scale]
 func New(hash []byte, gridSize int, scale int, palette color.Palette) image.Image {
-	ring := byteRing{bytes: hash}
-
 	img := image.NewRGBA(image.Rect(0, 0, gridSize*scale, gridSize*scale))
 
-	for x := 0; x < gridSize/2+gridSize%2; x++ {
+	i := 0
+
+	// x only needs to go halfway across (+1 if odd) because it will be mirrored
+	// horizontally:
+	maxX := gridSize/2 + gridSize%2
+	for x := 0; x < maxX; x++ {
 		for y := 0; y < gridSize; y++ {
-			c := int(ring.next()) % len(palette)
-			color := image.NewUniform(palette[c])
+			// Grab the next byte:
+			b := hash[i]
+			i++
+			i %= len(hash)
 
+			// Use the byte to pick a color from the palette:
+			color := palette[int(b)%len(palette)]
+			src := image.NewUniform(color)
+
+			// Draw this on the left side:
 			rect := image.Rect(x*scale, y*scale, (x+1)*scale, (y+1)*scale)
-			draw.Draw(img, rect, color, image.ZP, draw.Src)
+			draw.Draw(img, rect, src, image.ZP, draw.Src)
 
-			// Mirror horizontally:
+			// Mirror horizontally to the right side:
 			rect = image.Rect((gridSize-x-1)*scale, y*scale, (gridSize-x)*scale, (y+1)*scale)
-			draw.Draw(img, rect, color, image.ZP, draw.Src)
+			draw.Draw(img, rect, src, image.ZP, draw.Src)
 		}
 	}
 
 	return img
-}
-
-type byteRing struct {
-	bytes []byte
-	index int
-}
-
-func (r *byteRing) next() byte {
-	b := r.bytes[r.index]
-	r.index++
-	r.index %= len(r.bytes)
-	return b
-}
-
-func (r *byteRing) nextN(n int) []byte {
-	var out []byte
-	for i := 0; i < n; i++ {
-		out = append(out, r.next())
-	}
-	return out
-}
-
-func (r *byteRing) len() int {
-	return len(r.bytes)
-}
-
-func grid(rect image.Rectangle, numPixelBytes int) []image.Rectangle {
-	return []image.Rectangle{
-		image.Rect(0, 0, rect.Size().X/2, rect.Size().Y/2),
-		image.Rect(rect.Size().X/2, 0, rect.Size().X, rect.Size().Y/2),
-		image.Rect(0, rect.Size().Y/2, rect.Size().X/2, rect.Size().Y),
-		image.Rect(rect.Size().X/2, rect.Size().Y/2, rect.Size().X, rect.Size().Y),
-	}
 }
