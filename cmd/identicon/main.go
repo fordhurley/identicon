4345bf2e660afeb5aa948e148c7867afec5e8d1b
Ford Hurley <ford.hurley@gmail.com>
2019-03-16 12:08:37 -0400

    Add color theme flag

--- cmd/identicon/main.go
+++ cmd/identicon/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"image/color"
 	"image/png"
 	"log"
@@ -10,38 +11,47 @@ import (
 )
 
 func main() {
-	// TODO: args
+	var themeName string
+
+	flag.StringVar(&themeName, "theme", "rainbow", "select the color theme")
+	flag.Parse()
+
+	theme, ok := themes[themeName]
+	if !ok {
+		log.Fatal("invalid theme name:", themeName)
+	}
+
 	gridSize := 5
 	scale := 32
-	palettes := rainbow
 
-	img := identicon.New(os.Stdin, gridSize, scale, palettes)
+	img := identicon.New(os.Stdin, gridSize, scale, theme)
 	err := png.Encode(os.Stdout, img)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-var cool = []color.Palette{
-	{
-		color.RGBA{50, 178, 255, 255},
-		color.RGBA{163, 172, 177, 255},
-		color.RGBA{0, 0, 0, 255},
+var themes = map[string][]color.Palette{
+	"cool": []color.Palette{
+		{
+			color.RGBA{50, 178, 255, 255},
+			color.RGBA{163, 172, 177, 255},
+			color.RGBA{0, 0, 0, 255},
+		},
+		{
+			color.RGBA{255, 255, 255, 255},
+		},
 	},
-	{
-		color.RGBA{255, 255, 255, 255},
-	},
-}
-
-var rainbow = []color.Palette{
-	{
-		color.RGBA{170, 255, 0, 255},
-		color.RGBA{255, 170, 0, 255},
-		color.RGBA{255, 0, 170, 255},
-		color.RGBA{170, 0, 255, 255},
-		color.RGBA{0, 170, 255, 255},
-	},
-	{
-		color.RGBA{255, 255, 255, 255},
+	"rainbow": []color.Palette{
+		{
+			color.RGBA{170, 255, 0, 255},
+			color.RGBA{255, 170, 0, 255},
+			color.RGBA{255, 0, 170, 255},
+			color.RGBA{170, 0, 255, 255},
+			color.RGBA{0, 170, 255, 255},
+		},
+		{
+			color.RGBA{255, 255, 255, 255},
+		},
 	},
 }
